05fcf25f5d50b1e92fbd5060de177db876a3c26d
Amer Hallaq <114112833+HallaqAmer@users.noreply.github.com>
2024-04-06 01:21:48 +0200

    Revert "done migrations and converted to typescript waiting validations ready…"

---
+++ models/product.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init(
+    {
+      title: DataTypes.STRING,
+      subtitle: DataTypes.STRING,
+      description: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER,
+      discountpercentage: DataTypes.NUMBER,
+      initialquantity: DataTypes.NUMBER,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
+  return Product;
+};
