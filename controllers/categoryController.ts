d4ec07c04381fbfa50245ea71e31d63713fabfed
Amer Hallaq <amerhalaq91@gmail.com>
2024-04-17 16:40:16 +0200

    added ctegory endpoints

--- controllers/categoryController.ts
+++ controllers/categoryController.ts
@@ -14,25 +14,55 @@ const getAllCategories = async (req: Request, res: Response) => {
 
 // This method returns a specific category by ID
 const getCategoryById = async (req: Request, res: Response) => {
-  return;
-};
+  const categoryId = req.params.id;
+
+  db.Category.findByPk(categoryId)
+    .then((category: any) => {
+      if (!category) {
+        res.status(404).json({ error: "Category not found" });
+        return;
+      }
+      res.json({category});
+    })
+    .catch((error: Error) => {
+      // tslint:disable-next-line:no-console
+      console.error("Error finding Category:", error);
+      res.status(500).json({ error: "Internal server error" });
+  });
+}
 
 // This method returns all brands for a specific category id
 const getCategoryBrands = async (req: Request, res: Response) => {
-  const categoryId=req.params.id;
-  db.Category.findByPk(categoryId, {
-    include: [{model: db.Product,
-    attributes: ["brandId"],
-    include:{model:db.Brand}
-    }]
-  })
-  .then((category:any)=> {
-    res.json({category})
-  })
-  .catch((error:Error) => {
-    res.status(500).json({ error: 'Internal server error' });
-})
-  return;
+  const categoryId = req.params.id;
+  try {
+    const category = await db.Category.findByPk(categoryId, {
+      include: [
+        {
+          model: db.Product,
+          include: { model: db.Brand },
+        },
+      ],
+    });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const brands = category.Products.map((product: any) => ({
+      id: product.Brand.id,
+      name: product.Brand.name,
+      image: product.Brand.image,
+      createdAt: product.Brand.createdAt,
+    }));
+    const categoryWithBrands = {
+      ...category.toJSON(),
+      brands,
+    };
+    delete categoryWithBrands.Products;
+    res.json({ category: categoryWithBrands });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 // This method filters and returns products based on category id and brand id
